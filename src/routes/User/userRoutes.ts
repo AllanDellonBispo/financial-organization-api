d62b546ca794e09bdd7a7ea4ae3939d9bc2e5867
Allan Bispo <allandellon.desbra@gmail.com>
2024-10-27 18:35:44 -0300

    Remoção de trecho de código comentado

--- src/routes/User/userRoutes.ts
+++ src/routes/User/userRoutes.ts
@@ -3,12 +3,6 @@ import express from "express";
 
 const router = express.Router();
 
-// router.get('/teste', async(req: Request, res: Response) => {
-//     await User.find().then((data) =>{
-//         res.json(data);
-//     })
-// });
-
 router.get('/teste', userController.all);
 // router.post('/createTeste', userController.create);
 router.post('/login', userController.login);
